cf9c978e1e5ad4d30f58e6fca4e9aacda0a54f0b
tunastorm <ycw921@naver.com>
2024-05-24 16:44:11 +0900

    Initial Commit

---
+++ TableViewPractice2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewPractice2
+//
+//  Created by 유철원 on 5/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
